e4a73443dac0b710ef26f50620a45b1e5e70808f
Diego Sousa <diego7lts7@gmail.com>
2024-04-18 20:55:35 -0300

    adicionando os elementos do DOM no JS

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
-const audio = new Audio("sons/carSound.mp3");
-const backgroundJogo = document.querySelector("#backgroundJogo");
+// variaveis
 
-audio.loop = true;
-audio.currentTime = 3;
+//elemento de audio
+
+const audio = new Audio("sons/carSound.mp3");
+
+//elemento dos modais
+
+const modalInicioJogo = document.querySelector(".modalInicioJogo");
+const modalGameOver = document.querySelector(".gameOver");
+const modalIniciojogo = document.querySelector(".inicioJogo");
+const btnInicioJogo = document.querySelector("#botaoInicioJogo");
+
+//elementos do painel de jogo
+
+const modalScore = document.querySelector(".score");
+const scorePontuacao = document.querySelector("#pontuacao");
+const scoreRecorde = document.querySelector("#recorde");
+const painelJogo = document.querySelector(".painelJogo");
+const carMaquinaLeft = document.querySelector("#carMaquinaLeft");
+const carMaquinaRight = document.querySelector("#carMaquinaRigth");
+const carPlayer = document.querySelector("#carPlayer");
+
+// eventos
+
+
+// funções
