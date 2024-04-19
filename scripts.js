8a47b95a0e13facc5336713a607b4dabd394d35a
Diego Sousa <diego7lts7@gmail.com>
2024-04-18 21:38:08 -0300

    implementando no JS os eventos de iniciar jogo, audio. Implementei também o evento e a função de mover o carro com a setinha para a esquerda e direita.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@
 
 //elemento de audio
 
-const audio = new Audio("sons/carSound.mp3");
+const audio = new Audio("sons/carSound1.mp3");
 
 //elemento dos modais
 
-const modalInicioJogo = document.querySelector(".modalInicioJogo");
+const conteinerInicioJogo = document.querySelector(".modalInicioJogo");
 const modalGameOver = document.querySelector(".gameOver");
 const modalIniciojogo = document.querySelector(".inicioJogo");
 const btnInicioJogo = document.querySelector("#botaoInicioJogo");
@@ -21,7 +21,41 @@ const carMaquinaLeft = document.querySelector("#carMaquinaLeft");
 const carMaquinaRight = document.querySelector("#carMaquinaRigth");
 const carPlayer = document.querySelector("#carPlayer");
 
+// variaveis operacionais
+
+let left = false;
+
 // eventos
+
+btnInicioJogo.addEventListener("click", iniciaJogo);
 
 
 // funções
+
+function iniciaJogo() {
+    modalIniciojogo.style.display = "none";
+    carPlayer.style.display = "flex";
+    modalScore.style.display = "flex";
+    iniciaAudio();
+    document.addEventListener("keydown", carPlayerMoove);
+}
+
+function iniciaAudio(){
+    audio.loop = true;
+    audio.play();
+}
+
+function carPlayerMoove(event) {
+    if(event.key === "ArrowLeft"){
+        if(left === false){
+            carPlayer.style.animation = "animationCarPlayerLeft .4s forwards ease"
+            left = true;
+        }
+    }else if(event.key === "ArrowRight"){
+        if(left === true){
+            carPlayer.style.animation = "animationCarPlayerRigth .4s forwards ease"
+            left = false;
+        }
+    }
+    
+}
